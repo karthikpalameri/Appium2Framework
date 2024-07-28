e7e667729908c1e6e02b14253e285005993c0a14
Karthik Palameri <karthikbms17@gmail.com>
2024-07-28 10:59:55 +0530

    sorting module update

--- src/main/java/learn/java/ds/sorting/FindKthSmallestElement.java
+++ src/main/java/learn/java/ds/sorting/FindKthSmallestElement.java
@@ -1,2 +1,49 @@
-package learn.java.ds.sorting;public class FindKthSmallestElement {
+package learn.java.ds.sorting;
+
+public class FindKthSmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 2, 8, 3};
+        int k = 1;
+        int kthSmallestElement = findKthSmallestElement(arr, k);
+        System.out.println("kthSmallestElement = " + kthSmallestElement);
+    }
+
+    private static int findKthSmallestElement(int[] arr, int k) {
+        int n = arr.length;
+        int l = 0;
+        int h = n - 1;
+        int pivot = arr[h];
+        while (l <= h) {
+            int p = lomutoPartitionIt(arr, l, h);
+            if (p == k - 1) {
+                return k;
+            } else if (p > k - 1) {
+                h = p - 1;
+            } else {
+                l = p + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static int lomutoPartitionIt(int[] arr, int l, int h) {
+        int pivot = arr[h];
+        int i = l - 1;
+        for (int j = 0; j < h - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swapItFun(arr, i, j);
+            }
+        }
+        i++;
+        swapItFun(arr, i, h);
+        System.out.println("i = " + i);
+        return i;
+    }
+
+    private static void swapItFun(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
--- src/main/java/learn/java/ds/sorting/QuickSortDemo.java
+++ src/main/java/learn/java/ds/sorting/QuickSortDemo.java
@@ -1,2 +1,41 @@
-package learn.java.ds.sorting;public class QuickSortDemo {
+package learn.java.ds.sorting;
+
+import java.util.Arrays;
+
+public class QuickSortDemo {
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 7, 9, 3, 10, 5};
+        quickSortIt(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSortIt(int[] arr, int l, int h) {
+        if (l < h) {
+            int p = lomutoPartition(arr, l, h);
+            quickSortIt(arr, l, p - 1);
+            quickSortIt(arr, p + 1, h);
+        }
+        System.out.println("Quick Sort trace : Arrays.toString(arr) = " + Arrays.toString(arr));
+    }
+
+    private static int lomutoPartition(int[] arr, int l, int h) {
+        int pivot = arr[h];
+        int i = -1;
+        int n = arr.length;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swapIt(arr, i, j);
+            }
+        }
+        i++;
+        swapIt(arr, i, h);
+//        System.out.println("lomuto res: " + Arrays.toString(arr));
+        return i; //return the pivot index
+    }
+
+    private static void swapIt(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
