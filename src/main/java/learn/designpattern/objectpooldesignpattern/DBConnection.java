f89f3e72f7b61f7f2f78794dfea93d5b8ace4988
Karthik Palameri <karthikbms17@gmail.com>
2024-07-29 10:13:49 +0530

    DBConnection learn code update

---
+++ src/main/java/learn/designpattern/objectpooldesignpattern/DBConnection.java
@@ -0,0 +1,53 @@
+package learn.designpattern.objectpooldesignpattern;
+
+import lombok.Getter;
+
+import java.sql.*;
+
+@Getter
+public class DBConnection {
+    Connection connection;
+
+    public DBConnection(String url) throws SQLException, ClassNotFoundException {
+        // Load the SQLite JDBC driver
+        Class.forName("org.sqlite.JDBC");
+        this.connection = DriverManager.getConnection(url);
+    }
+
+    public void closeConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public void closeStatement(Statement statement) throws SQLException {
+        if (statement != null) {
+            statement.close();
+        }
+    }
+
+    public void closePreparedStatement(PreparedStatement preparedStatement) throws SQLException {
+        if (preparedStatement != null) {
+            preparedStatement.close();
+        }
+    }
+
+    public void closeResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet != null) {
+            resultSet.close();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM Album where AlbumId = ?";
+        try (Connection connection = new DBConnection("jdbc:sqlite:/Users/karthikp/Library/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db").getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setInt(1, 5);
+            try (ResultSet resultSet = preparedStatement.executeQuery();) {
+                while (resultSet.next()) {
+                    String res = resultSet.getInt("AlbumId") + " " + resultSet.getString("Title");
+                    System.out.println("res = " + res);
+                }
+            }
+        }
+    }
+}
