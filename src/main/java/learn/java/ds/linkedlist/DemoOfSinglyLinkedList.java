292a6ae894ed83d621bc798aac9edacdfca59eb2
Karthik Palameri <karthikbms17@gmail.com>
2025-01-26 11:46:11 +0530

    linked list learn update3

--- src/main/java/learn/java/ds/linkedlist/DemoOfSinglyLinkedList.java
+++ src/main/java/learn/java/ds/linkedlist/DemoOfSinglyLinkedList.java
@@ -58,6 +58,41 @@ public class DemoOfSinglyLinkedList {
 
     }
 
+    public static Node delHead(Node head) {
+        if (head == null) {
+            return null;
+        }
+        head = head.next;
+        return head;
+    }
+
+    public static Node delLast(Node head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return null;
+        Node curr = head;
+        // 10 20 30
+        while (curr.next.next != null)
+            curr = curr.next;
+        curr.next = null;
+        return head;
+    }
+
+    public static int search(Node head, int x) {
+        int pos = 1;
+        Node curr = head;
+        while (curr != null) {
+            if (curr.data == x) {
+                return pos;
+            } else {
+                pos = pos + 1;
+                curr = curr.next;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Node head = null;
         head = insertBegin(head, 30);
@@ -66,12 +101,16 @@ public class DemoOfSinglyLinkedList {
 
         head = null;
         head = insertEnd(head, 40);
-//        head = insertEnd(head, 50);
-//        head = insertEnd(head, 60);
+        head = insertEnd(head, 50);
+        head = insertEnd(head, 60);
 
 //        head = null;
         head = insertAtPosition(head, 4, 100);
 
+        head = delHead(head);
+        head = delLast(head);
+
+        int search = search(head, 40);
         System.out.println("head = " + head);
     }
 }
