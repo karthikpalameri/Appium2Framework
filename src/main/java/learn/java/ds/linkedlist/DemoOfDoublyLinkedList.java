6947248c29c2b9d9c6bbef69bb0314eb79fb1cbb
Karthik Palameri <Karthik.Palameri@bottomline.com>
2025-01-27 14:34:48 +0530

    Doubly linked list learn 2

--- src/main/java/learn/java/ds/linkedlist/DemoOfDoublyLinkedList.java
+++ src/main/java/learn/java/ds/linkedlist/DemoOfDoublyLinkedList.java
@@ -42,6 +42,61 @@ public class DemoOfDoublyLinkedList {
         return head;
     }
 
+    static Nodee reverseDoublyLinkedList(Nodee head) {
+        //corner case handle 
+        if (head == null)
+            return head;
+        if (head.next == null)
+            return head;
+
+        //init prev and next as null
+        Nodee prev = null;
+        Nodee next = null;
+        Nodee curr = head;
+
+        //   n
+        // 10 20 30 
+        //   
+        // h
+        // c
+
+        //Initial 
+        // null<->[10|prev:null, next:20] <-> [20|prev:10, next:30]<-> [30|prev:20, next:null]<->null
+
+/*
+         prev = null        
+         null<->[10|prev:null, next:20] <-> [20|prev:10, next:30]<-> [30|prev:20, next:null]<->null
+         curr
+         
+                                            prev = 20
+         null<->[10|prev:20, next:null] <-> [20|prev:10, next:30]<-> [30|prev:20, next:null]<->null
+                                            curr
+         
+         
+                                                                     prev = 30
+         null<->[10|prev:20, next:null] <-> [20|prev:30, next:10]<-> [30|prev:20, next:null]<->null
+                                                                     curr
+
+                                             
+                                                                     prev = 30
+         null<->[10|prev:20, next:null] <-> [20|prev:30, next:10]<-> [30|prev:20, next:null]<->null
+                                                                     curr
+*/
+
+
+        while (curr.next != null) {
+            //swap pointer
+            //swap curr.next with curr.prev
+            prev = curr.next;
+            curr.next = curr.prev;
+            curr.prev = prev;
+
+            //move the curr to right
+            curr = curr.prev;// note we reversed the pointers
+        }
+        return prev.prev;
+    }
+
 
     public static void main(String[] args) {
         Nodee head = null;
@@ -50,6 +105,9 @@ public class DemoOfDoublyLinkedList {
 
         head = insertEnd(head, 30);
         head = insertEnd(head, 40);
+
+        //reverse doubly linked list 
+        head = reverseDoublyLinkedList(head);
         System.out.println("head = " + head);
     }
 }
