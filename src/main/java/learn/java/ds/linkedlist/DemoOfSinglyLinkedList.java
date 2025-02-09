339b64b033b739477f727342610f11a9d6f93e14
Karthik Palameri <karthikbms17@gmail.com>
2025-02-09 19:13:06 +0530

    Singly linked list learn update4

--- src/main/java/learn/java/ds/linkedlist/DemoOfSinglyLinkedList.java
+++ src/main/java/learn/java/ds/linkedlist/DemoOfSinglyLinkedList.java
@@ -93,24 +93,61 @@ public class DemoOfSinglyLinkedList {
         return -1;
     }
 
+    private static Node sortedInsertIntoSinglyLinkedList(Node head, int d) {
+        Node temp = new Node(d);
+        //checks
+        //if empty
+        if (head == null) {
+            return null;
+        }
+        //if data is less than 1st elem
+        if (d < head.data) {
+            //insert data at 1st position and link
+            //    10   20    30
+            //    h
+            // 5
+            // d
+            // t
+            temp.next = head;
+            return temp;
+        }
+
+        //10    20    30
+        //h
+        //         25
+        //         t
+        //      c
+        //traverse till 1 step back where the data need to be inserted
+        Node curr = head;
+        while (curr.next != null && curr.next.data < d) {
+            curr = curr.next;
+        }
+        System.out.println("curr pointer is now pointing to , curr.data = " + curr.data);
+
+        curr.next = temp.next;
+        curr.next = temp;
+
+        return head;
+    }
+
     public static void main(String[] args) {
         Node head = null;
         head = insertBegin(head, 30);
         head = insertBegin(head, 20);
         head = insertBegin(head, 10);
 
-        head = null;
-        head = insertEnd(head, 40);
-        head = insertEnd(head, 50);
-        head = insertEnd(head, 60);
-
 //        head = null;
-        head = insertAtPosition(head, 4, 100);
+//        head = insertEnd(head, 40);
+//        head = insertEnd(head, 50);
+//        head = insertEnd(head, 60);
 
-        head = delHead(head);
-        head = delLast(head);
-
-        int search = search(head, 40);
+//        head = insertAtPosition(head, 4, 100);
+//
+//        head = delHead(head);
+//        head = delLast(head);
+//
+//        int search = search(head, 40);
+        sortedInsertIntoSinglyLinkedList(head, 5);
         System.out.println("head = " + head);
     }
 }
